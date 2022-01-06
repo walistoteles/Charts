903c0d4a5d6aa1c7fde37ce06d810ce7fe5b7f23
Wali <Wali@DESKTOP-36JIRUG>
2022-01-06 10:59:26 -0300

    Adicionar arquivos de projeto.

---
+++ MicroCharts/MicroCharts/Model/SaleInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroCharts.Model
+{
+    class SaleInfo
+    {
+
+
+
+        public int Year { get; set; }
+        public double Target { get; set; }
+        public double Sale { get; set; }
+    }
+}
---
+++ MicroCharts/MicroCharts/ViewModel/SaleInfoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MicroCharts.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroCharts.ViewModel
+{
+    class SaleInfoViewModel
+    {
+
+        public List<SaleInfo> SalesDatas { get; set; }
+
+
+        public SaleInfoViewModel()
+        {
+            SalesDatas = new List<SaleInfo>();
+
+
+            SalesDatas.Add(new SaleInfo { Year = 2021, Target = 500, Sale = 342 });
+            SalesDatas.Add(new SaleInfo { Year = 2022, Target = 520, Sale = 320 });
+            SalesDatas.Add(new SaleInfo { Year = 2023, Target = 260, Sale = 360 });
+            SalesDatas.Add(new SaleInfo { Year = 2024, Target = 600, Sale = 430 });
+            SalesDatas.Add(new SaleInfo { Year = 2025, Target = 200, Sale = 560 });
+        }
+    }
+}
